c92060a25a8620786ab2592ce75e1e8859441a23
madison mirelez <madisonmirelez@madisons-MacBook-Pro.local>
2024-08-03 17:16:24 -0500

    added images and working on nav bar

---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+//import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="logo"
+          />
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
