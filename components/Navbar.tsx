cdef021c4e4834f034fd3ae82f339d1b38c626ff
madison mirelez <madisonmirelez@madisons-MacBook-Pro.local>
2024-08-04 16:17:10 -0500

    added logos and finished navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,12 @@ import Link from "next/link";
 import Image from "next/image";
 //import React from "react";
 
+const navIcons = [
+  { src: "/assets/icons/search.svg", alt: "search" },
+  { src: "/assets/icons/black-heart.svg", alt: "heart" },
+  { src: "/assets/icons/user.svg", alt: "user" },
+];
+
 const Navbar = () => {
   return (
     <header className="w-full">
@@ -13,7 +19,24 @@ const Navbar = () => {
             height={27}
             alt="logo"
           />
+
+          <p className="nav-logo">
+            Price<span className="text-primary"> Wise</span>
+          </p>
         </Link>
+
+        <div className="flex items-center gap-5">
+          {navIcons.map((icon) => (
+            <Image
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              width={28}
+              height={28}
+              className="object-contain"
+            />
+          ))}
+        </div>
       </nav>
     </header>
   );
